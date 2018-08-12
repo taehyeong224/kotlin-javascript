9951d5831e4924a9c7eafc1b8f05f3a7e3eaf9f5
김태형 <taehyeong224@gmail.com>
2018-08-12 22:32:44 +0900

    create button

--- out/production/kotlin-javascript/kotlin-javascript.js
+++ out/production/kotlin-javascript/kotlin-javascript.js
@@ -3,10 +3,27 @@ if (typeof kotlin === 'undefined') {
 }
 this['kotlin-javascript'] = function (_, Kotlin) {
   'use strict';
+  var trimIndent = Kotlin.kotlin.text.trimIndent_pdl1vz$;
   var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var ensureNotNull = Kotlin.ensureNotNull;
+  var Unit = Kotlin.kotlin.Unit;
+  function main$lambda(it) {
+    window.alert('\uCEAC\uCEC4\uCEAC');
+    return Unit;
+  }
   function main(args) {
     var message = 'Hello JavaScript!';
+    var num = 10;
+    var header = trimIndent('\n        <h1>Hello World<\/h1>\n        <button id="b1">\uC548\uB155<\/button>\n        10\n    ');
     println(message);
+    var root = document.getElementById('root');
+    ensureNotNull(root).innerHTML = '<div>hi<div>';
+    println(root);
+    var rootHeader = document.getElementById('root-header');
+    ensureNotNull(rootHeader).innerHTML = header;
+    println(rootHeader);
+    var b1 = document.getElementById('b1');
+    ensureNotNull(b1).addEventListener('click', main$lambda);
   }
   _.main_kand9s$ = main;
   main([]);
--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,26 @@
+import kotlin.browser.document
+import kotlin.browser.window
+
 fun main(args: Array<String>) {
     val message = "Hello JavaScript!"
+    val num = 10
+    val header = """
+        <h1>Hello World</h1>
+        <button id="b1">안녕</button>
+        $num
+    """.trimIndent()
     println(message)
+
+    val root = document.getElementById("root")
+    root!!.innerHTML = "<div>hi<div>"
+    println(root)
+
+    val rootHeader = document.getElementById("root-header")
+    rootHeader!!.innerHTML = header
+    println(rootHeader)
+
+    val b1 = document.getElementById("b1")
+    b1!!.addEventListener("click", {
+        window.alert("캬컄캬")
+    })
 }
